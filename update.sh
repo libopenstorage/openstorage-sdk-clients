a80f18b42c986efb73320a60935fd30eb1a8a453
Luis Pabón <luis@portworx.com>
2019-04-04 20:59:45 -0400

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+branches="master:master release-0.42:release-6.0 release-0.22:release-4.0 release-sdk-0.9:release-sdk-0.9"
+for branch in $branches ; do
+	# Get branches
+	lb=$(echo $branch | cut -d: -f1)
+	rb=$(echo $branch | cut -d: -f2)
+
+	# Switch to branch
+	git checkout $lb
+
+	# Get versios of current and new
+	prevver=$(cat api.swagger.json | jq -r '.info.version')
+	curl https://raw.githubusercontent.com/libopenstorage/openstorage/${rb}/api/server/sdk/api/api.swagger.json \
+		--output api.swagger.json --silent
+	ver=$(cat api.swagger.json | jq -r '.info.version')
+
+	if [ "$prevver" != "$ver" ] ; then
+		echo ">>> Updating branch $lb from $prevver to $ver"
+		sleep 2
+		make
+		git add api.swagger.json
+		git add sdk/.
+		git commit -am "Update to $ver"
+	else
+		echo ">>> Branch $lb does not need an update. At ver $ver"
+	fi
+done
+git checkout master
