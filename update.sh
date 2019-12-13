2b5674c60cf44ccd99208f6b08a9bde5dfa39d99
Luis Pabón <luis@portworx.com>
2019-12-12 22:43:31 -0800

    update.sh

--- update.sh
+++ update.sh
@@ -5,19 +5,12 @@ fail() {
 	exit 1
 }
 
-branches="master:master release-0.42:release-6.2 release-0.22:release-4.0 release-sdk-0.9:release-sdk-0.9"
-localbranch=$(git symbolic-ref --short HEAD)
-filter="$1"
+create() {
+	local lb=$1
+	local rb=$2
 
-for branch in $branches ; do
-	# Get branches
-	lb=$(echo $branch | cut -d: -f1)
-	rb=$(echo $branch | cut -d: -f2)
-
-	# Check filter
-	if [ "$filter" != "all" -a "$localbranch" != "$lb" ] ; then
-		continue
-	fi
+	[ -n "$lb" ] || fail "Missing lb"
+	[ -n "$rb" ] || fail "Missing rb"
 
 	# Switch to branch
 	git checkout $lb
@@ -32,7 +25,7 @@ for branch in $branches ; do
 	if [ "$prevver" != "$ver" ] ; then
 		echo ">>> Updating branch $lb from $prevver to $ver"
 		sleep 2
-		make || fail "Make filed"
+		make || fail "Make failed"
 		git add api.swagger.json
 		git add sdk/.
 		git commit -am "Update to $ver"
@@ -40,6 +33,24 @@ for branch in $branches ; do
 	else
 		echo ">>> Branch $lb does not need an update. At ver $ver"
 		git checkout api.swagger.json
+	fi
+}
+
+branches="master:master release-0.42:release-6.2 release-0.22:release-4.0 release-sdk-0.9:release-sdk-0.9"
+localbranch=$(git symbolic-ref --short HEAD)
+filter="$1"
+
+for branch in $branches ; do
+	# Get branches
+	lb=$(echo $branch | cut -d: -f1)
+	rb=$(echo $branch | cut -d: -f2)
+
+	if [ "$lb" = "$filter" ] ; then
+		create "$lb" "$rb"
+	elif [ "$filter" = "all" ] ; then
+		create "$lb" "$rb"
+	elif [ -z "$filter" -a "$localbranch" = "$lb" ] ; then
+		create "$lb" "$rb"
 	fi
 done
 
