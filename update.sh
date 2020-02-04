26b88179070b6a95366fe534db953f68452aaaff
Luis Pabón <luis@portworx.com>
2020-02-04 11:35:47 -0800

    api.swagger.json and added update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+fail() {
+	echo "$1"
+	exit 1
+}
+
+create() {
+	local rb=$1
+
+	[ -n "$rb" ] || fail "Missing rb"
+
+	prevver=$(cat api.swagger.json | jq -r '.info.version')
+	curl https://raw.githubusercontent.com/libopenstorage/openstorage/${rb}/api/server/sdk/api/api.swagger.json \
+		--output api.swagger.json --silent
+	ver=$(cat api.swagger.json | jq -r '.info.version')
+
+	if [ "$prevver" != "$ver" ] ; then
+		echo ">>> Updating branch from $prevver to $ver"
+		sleep 2
+		make APIVER=${ver} || fail "Make failed"
+		#git add api.swagger.json
+		#git add sdk/.
+		#git commit -am "Update to $ver"
+		#sudo git clean -f -x -d
+	else
+		echo ">>> Branch does not need an update. At ver $ver"
+		git checkout api.swagger.json
+	fi
+}
+
+create "release-6.3"
+
